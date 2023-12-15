c45b063e60bbc96ea3c7db62c922aec1dca27bbe
ngochuyen <ngochuyen2k17@gmail.com>
2023-12-15 21:11:25 +0700

    first commit

---
+++ exercise/src/App.js
@@ -0,0 +1,12 @@
+import Router from "./react1/routes";
+
+function App() {
+  return (
+    <div className="App">
+      {/* React 1 */}
+      { <Router /> }
+    </div>
+  );
+}
+
+export default App;
